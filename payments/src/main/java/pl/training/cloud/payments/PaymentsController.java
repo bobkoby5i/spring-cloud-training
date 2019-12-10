4129e6347c75040bdc57a52903e0e87487816c2c
landrzejewski <landrzejewski.poczta@gmail.com>
2019-12-10 10:47:42 +0100

    payemnts

--- payments/src/main/java/pl/training/cloud/payments/PaymentsController.java
+++ payments/src/main/java/pl/training/cloud/payments/PaymentsController.java
@@ -1,16 +1,30 @@
 package pl.training.cloud.payments;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.training.cloud.payments.api.PaymentResponseDto;
+import pl.training.cloud.payments.api.PaymentsRequestDto;
 
 @RequestMapping("payments")
 @RestController
+@RequiredArgsConstructor
 public class PaymentsController {
 
-    @GetMapping
-    public String test() {
-        return "OK";
+    private final FakePaymentsService fakePaymentsService;
+    private final CreditCardMapper creditCardMapper;
+    private final PaymentMapper paymentMapper;
+
+    @PostMapping
+    public ResponseEntity<PaymentResponseDto> addPaymentRequest(PaymentsRequestDto paymentsRequestDto) {
+        Long amount = paymentsRequestDto.getAmount();
+        CreditCard creditCard = creditCardMapper.toCreditCard(paymentsRequestDto.getCard());
+        Payment payment = fakePaymentsService.pay(amount, creditCard);
+        PaymentResponseDto paymentResponseDto = paymentMapper.toPaymentResponseDto(payment);
+        return ResponseEntity.accepted().body(paymentResponseDto);
     }
 
 }
