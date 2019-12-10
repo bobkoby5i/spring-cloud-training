b1412596736b90ed7249f8e410cac7606ae2aeba
landrzejewski <landrzejewski.poczta@gmail.com>
2019-12-10 09:08:36 +0100

    payemnts

--- trips/src/main/java/pl/training/cloud/trips/TripsController.java
+++ trips/src/main/java/pl/training/cloud/trips/TripsController.java
@@ -1,12 +1,18 @@
 package pl.training.cloud.trips;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("trips")
 @RestController
+@RequiredArgsConstructor
 public class TripsController {
+
+    private final TripsService tripsService;
+    private final TripMapper tripMapper;
+
 
     @GetMapping
     public String test() {
