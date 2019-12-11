fb8da5e5df39572f3c13f675e96bb50056e3e903
landrzejewski <landrzejewski.poczta@gmail.com>
2019-12-11 15:46:02 +0100

    payemnts

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@
 docker rmi $(docker images | grep "<none>")
 
 docker build -t "training/configuration" configuration
+docker build -t "training/discovery" discovery
+docker build -t "training/gateway" gateway
+docker build -t "training/zipkin" zipkin
+docker build -t "training/admin" admin
+docker build -t "training/payments" payments
+docker build -t "training/trips" trips
