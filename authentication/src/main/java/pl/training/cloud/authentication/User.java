eb890f87ac040b3ae23159ae9eabd1c884e6250f
landrzejewski <landrzejewski.poczta@gmail.com>
2019-12-13 14:33:52 +0100

    payemnts

--- authentication/src/main/java/pl/training/cloud/authentication/User.java
+++ authentication/src/main/java/pl/training/cloud/authentication/User.java
@@ -21,6 +21,7 @@ public class User implements UserDetails {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+    @Column(unique = true)
     private String username;
     private String password;
     private boolean enabled;
